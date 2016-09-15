06f4d211a0db358df5bee97306d32ccff3cc3cf5
Matt Moore <mattmoore@carbonhelix.com>
2016-09-15 03:15:11 -0500

    Fix main.go test code.

--- main.go
+++ main.go
@@ -5,27 +5,23 @@ import "os"
 import "strings"
 import "github.com/mattmoore/novo/interpreter"
 
-// import "github.com/mattmoore/novo/schema"
+import "github.com/mattmoore/novo/schema"
 
 func main() {
 	command := strings.Join(os.Args[1:], " ")
 	response := interpreter.ExecCommand(command)
 	fmt.Println(response)
 
-	// schema1 := new(schema.Schema)
-	//
-	// db1 := schema.Database{Name: "db1"}
-	// schema1.Databases = append(schema1.Databases, &db1)
-	//
+	// db := schema.Database{Name: "db1"}
 	// table1 := schema.Table{
-	// 	Name: "table1",
+	// 	Name:        "table1",
+	// 	PrimaryKeys: []string{"id"},
 	// 	Columns: []*schema.Column{
-	// 		&schema.Column{Name: "id", Type: "serial", PrimaryKey: true},
+	// 		&schema.Column{Name: "id", Type: "serial"},
 	// 		&schema.Column{Name: "col1", Type: "varchar(100)"},
 	// 	},
 	// }
-	//
-	// db1.Tables = append(db1.Tables, &table1)
-	//
-	// fmt.Print(schema1.GetSchemaSql())
+	// db.Tables = append(db.Tables, &table1)
+	// db.Compile()
+	// fmt.Println(db.Sql())
 }
